5851d809bc4d069eef0e9f7dac952fd55016b2da
Loic Guerin <guerin.loic@gmail.com>
2025-09-10 10:41:02 +0200

    feature: modal can be closed by a click on (x) icon #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,15 +9,24 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalbgclose = document.querySelector(".bground .close");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+modalbgclose.addEventListener("click", () => {
+  closeModal();
+});
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// launch modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
